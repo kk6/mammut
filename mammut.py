c029d3df3d4d08e339a7254d36e5d7bf91189008
kk6 <hiro.ashiya@gmail.com>
2017-04-17 13:37:10 +0900

    Add function register_app.

---
+++ mammut.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import json
+from urllib.parse import urljoin
+
+import requests
+
+
+def register_app(client_name, base_url, redirect_uris='urn:ietf:wg:oauth:2.0:oob',
+                 scopes='read write follow', website=None, filepath=None):
+    """Register application
+    
+    :param client_name: Name of your application
+    :param base_url: Specify the base URL of the Mastodon instance you want to connect. Example: https://mstdn.jp
+    :param redirect_uris: Where the user should be redirected after authorization
+        (for no redirect, `use urn:ietf:wg:oauth:2.0:oob`)
+    :param scopes: This can be a space-separated list of the following items: "read", "write" and "follow"
+    :param website: (optional) URL to the homepage of your app
+    :param filepath: (optional) Specify the file path if you want to save the acquired information in a JSON file.
+    :return: Returns `id`, `client_id` and `client_secret`
+    :rtype: dict
+    
+    Usage::
+    
+        >>> register_app('myapp', 'https://mstdn.jp')
+        {'id': 1234, 'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob', 'client_id': '...', 'client_secret': '...'}
+    
+    """
+    data = {
+        'client_name': client_name,
+        'redirect_uris': redirect_uris,
+        'scopes': scopes,
+        'website': website,
+    }
+    url = urljoin(base_url, '/api/v1/apps')
+    resp = requests.post(url, data=data)
+    resp.raise_for_status()
+    data = resp.json()
+    if filepath:
+        with open(filepath, 'w') as fp:
+            json.dump(data, fp, indent=2, sort_keys=True)
+    return data
