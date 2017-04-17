28ee750e974201f6566928c3347a929ded741ba7
kk6 <hiro.ashiya@gmail.com>
2017-04-17 14:00:22 +0900

    Add OAuth2Handler.

--- mammut.py
+++ mammut.py
@@ -3,8 +3,9 @@ import json
 from urllib.parse import urljoin
 
 import requests
+from requests_oauthlib import OAuth2Session
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 def register_app(client_name, base_url, redirect_uris='urn:ietf:wg:oauth:2.0:oob',
@@ -41,3 +42,52 @@ def register_app(client_name, base_url, redirect_uris='urn:ietf:wg:oauth:2.0:oob
         with open(file_path, 'w') as fp:
             json.dump(data, fp, indent=2, sort_keys=True)
     return data
+
+
+class OAuth2Handler:
+    """OAuth2 authentication handler
+
+    :param client_id: Your Client ID
+    :param client_secret: Your Client Secret
+    :param base_url: Specify the base URL of the Mastodon instance you want to connect. Example: https://mstdn.jp
+    :param scope: This can be a space-separated list of the following items: "read", "write" and "follow"
+    :type scope: list of str
+    :param redirect_uri: Where the user should be redirected after authorization
+        (for no redirect, `use urn:ietf:wg:oauth:2.0:oob`)
+
+    """
+    def __init__(self, client_id, client_secret, base_url, scope=('read',), redirect_uri='urn:ietf:wg:oauth:2.0:oob'):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.base_url = base_url,
+        self.redirect_uri = redirect_uri
+        self.scope = scope
+        self._oauth = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, scope=scope)
+
+    def get_authorization_url(self):
+        """Get authorization URL
+        
+        :return: Authorization URL and state
+        :rtype: tuple
+
+        """
+        url = urljoin(self.base_url, '/oauth/authorize')
+        authorization_url, state = self._oauth.authorization_url(url)
+        return authorization_url, state
+
+    def fetch_token(self, code, file_path=None):
+        """Fetch token
+        
+        :param code: Authorization code
+        :param file_path: (optional) Specify the file path if you want to save the acquired information in a JSON file.
+        :return: Returns `access_token`, `token_type`, `scope` and `created_at`.
+        :rtype: dict
+
+        """
+        token_url = urljoin(self.base_url, '/oauth/token')
+        token = self._oauth.fetch_token(token_url, authorization_response=code, client_id=self.client_id,
+                                        client_secret=self.client_secret, code=code)
+        if file_path:
+            with open(file_path, 'w') as fp:
+                json.dump(token, fp, indent=2, sort_keys=True)
+        return token
